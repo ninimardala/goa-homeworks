e4ab11586db6f25b54d3142798e33ac0fc207296
ninimardala <ninimardaleishvili2023@gmail.com>
2024-07-06 11:59:59 +0400

    commit

---
+++ Day 22/Homework/task.py
@@ -0,0 +1,15 @@
+""" დავალება1:
+ეცადეთ შექმნათ სარეგისტრაციო ფორმა,
+შექმენით ინფუთები, რომლითაც მომხმარებელს მოსთხოვთ gmail-ს,
+და
+password-ს.
+ასევე გამოიყენეთ and და or ოპერატოები."""
+
+
+Name = (input("enter your name: "))
+mail = (input("enter your mail: "))
+password= (input("enter your password: "))
+
+print(type(Name))
+print(type(mail))
+print(type(password))
---
+++ Day 22/Homework/task2.py
@@ -0,0 +1,17 @@
+"""დავალება3:
+ჩამოწერეთ რამოდენიმე ინფუთი,
+და შემოიტანეთ თქვენი სკოლის ნიშნები,
+შემდეგ კი გამოიტანეთ ამ რიცხვების საშაუალოარითმეტიკული."""
+
+
+
+
+Georgian = (input("Write Georgian score: "))
+math = (input("Enter math score: "))
+art= (input("Write the score of art: "))
+English = (input("Write English Score:"))
+
+print(type(Georgian))
+print(type(math))
+print(type(art))
+print(type(English))
