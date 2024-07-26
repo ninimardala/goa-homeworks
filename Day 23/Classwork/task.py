faef11bbc5c57ddecddfbcec3bc68f0bebd7cdfc
ninimardala <ninimardaleishvili2023@gmail.com>
2024-07-26 21:46:18 +0400

    commit

---
+++ Day 23/Classwork/task.py
@@ -0,0 +1,5 @@
+def chak_number(num):
+    if 5 < num < 20:
+        return num
+    else:
+        False
---
+++ Day 24/Homework/task2.py
@@ -0,0 +1,12 @@
+"""დავალება: მომხარებელს შემოატანინეთ სახელი,
+შემდეგ ეგ სახელი ჩასვით ლისტში და შემდეგ დაპრინტეთ ეგ სახელი,
+გაითვალისწინეთ რომ არ უნდა იყოს 1 სახელი ლისტში,
+რამოდენიმე სახელი ჩაწერეთ,
+ხოლო დაუპრინტეთ ის სახელი რომელიც მომხმარებელი შეიყვანს"""
+
+name = input("enter your name: ")
+lastname = input("enter your name: ")
+age = input("enter your age: ")
+color = input("enter your favorite color: ")
+data = [name, lastname, age, color,]
+print (data)
