268fd16a89fcf7f741df77123543d5a59ccf3634
ninimardala <ninimardaleishvili2023@gmail.com>
2024-08-12 23:58:06 +0400

    commit

---
+++ Day 27/Classwork/task2.py
@@ -0,0 +1,13 @@
+num = int(input("enter number: "))
+num = int(input("enter number: "))
+num = int(input("enter number: "))
+num = int(input("enter number: "))
+num = int(input("enter number: "))
+for i in range(num , 50 , 2):
+    print (i)
+
+for i in range(num , 50 , 2):
+    print (i)
+    
+if num > 50:
+    print("საკმარისია, რიცხვი მეტია 50-ზე.")
---
+++ Day 27/Homework/task.py
@@ -0,0 +1,47 @@
+#1
+name = input("enter your name: ")
+surname = input("enter your surname: ")
+age = int(input("enter your age: "))
+
+print(type(name))
+print(type(surname))
+print(type(age))
+
+
+#2
+num1 = int(input("enter num: "))
+num2 = int(input("enter num: "))
+num3 = int(input("enter num: "))
+num4 = int(input("enter num: "))
+num5 = int(input("enter num: "))
+
+
+print (num1 + num2)
+print (num5 - num4)
+print (num3 * num2)
+print (num3 / num5)
+
+
+
+#3
+num1 = int(input("enter num: "))
+num2 = int(input("enter num: "))
+
+
+print (num1 + num2)
+print (num1 - num2)
+print (num1 * num2)
+print (num1 / num2)
+
+#4
+name = input("enter username: ")
+surname = input("enter your gmair or numbers: ")
+age = input("enter your password: ")
+
+print ("login is completel")
+
+#5
+num6 = input("enter numbers: ")
+num7 = num6 + 20
+
+print (num7)
